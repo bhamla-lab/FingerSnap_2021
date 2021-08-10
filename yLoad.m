706da749f60e59e3ace84bc49eb21d7482be0414
Raghav Acharya <50839181+RaghavAcharya@users.noreply.github.com>
2021-08-09 23:31:39 -0400

    Delete yLoad.m

--- yLoad.m
+++
@@ -1,9 +0,0 @@
-function [y] = yLoad(R,x)
-%YLATCH Takes in Latch geometry and latch position and outputs load
-%position.
-%   Equation derived from formula for a circle
-    n=2;
-    y = R*(1-(1-(x/R).^n).^(1./n));
-
-end
-

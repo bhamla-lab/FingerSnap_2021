6dcde1068f38e61cf5e63c5d358294257ba78997
Raghav Acharya <50839181+RaghavAcharya@users.noreply.github.com>
2021-08-09 23:32:09 -0400

    Delete dyLoad.m

--- dyLoad.m
+++
@@ -1,9 +0,0 @@
-function [dy] = dyLoad(R,x,dx)
-%DYLOAD Takes in Latch properties, position of latch, and velocity of latch
-%and outputs the speed of the load.
-%   Derivative of function in yLoad
-    n=2;
-    dy = R*(x^(n-1)*dx/R^n)*(1-(x/R)^n)^(1/n-1);
-
-end
-
